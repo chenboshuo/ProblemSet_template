1bdbb4e04c0f034a30353e44795bd17690bad63c
chenboshuo <chenboshuo.hi@163.com>
2020-10-15 21:52:19 +0800

    :sparkles: manage the todo list.

--- make.sh
+++ make.sh
@@ -1,13 +1,27 @@
+# make the build directory
 mkdir _build
-echo "\documentclass[ebook,fleqn]{problemset}" > ebook.tex | awk 'FNR>1' main.tex >> ebook.tex
+
+# make ebook file
+echo "\PassOptionsToPackage{disable}{todonotes} \documentclass[ebook,fleqn]{problemset}" > ebook.tex
+awk 'FNR>2' main.tex >> ebook.tex
 xelatex ebook.tex
 xelatex ebook.tex
 mv ebook.pdf _build/ebook.pdf
 
-echo "\documentclass{problemset}" > plain.tex | awk 'FNR>1' main.tex >> plain.tex
+# make plain book file
+echo "\PassOptionsToPackage{disable}{todonotes} \documentclass{problemset}" > plain.tex
+awk 'FNR>2' main.tex >> plain.tex
 xelatex plain.tex
 xelatex plain.tex
 mv plain.pdf _build/plain.pdf
 
+# make books with todos
+echo "\documentclass{problemset}" > todo.tex
+awk 'FNR>2' main.tex >> todo.tex
+xelatex todo.tex
+xelatex todo.tex
+mv todo.pdf _build/todo.pdf
+
 rm ./ebook.tex
 rm ./plain.tex
+rm ./todo.tex
