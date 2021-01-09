91aeb505c3af13d18f3b556f64fa7653172390a1
chenboshuo <chenboshuo.hi@163com>
2021-01-09 10:29:47 +0800

    :sparkles: add the solution to add chapter reference.

--- make.sh
+++ make.sh
@@ -1,28 +1,31 @@
+# define a build
+build_item(){
+  xelatex $1.tex
+  makeglossaries $1
+  biber $1
+  xelatex $1.tex
+  xelatex $1.tex
+}
+
 # make the build directory
 mkdir _build
 
 # make ebook file
 echo "\PassOptionsToPackage{disable}{todonotes} \documentclass[ebook,fleqn]{problemset}" > ebook.tex
 awk 'FNR>2' main.tex >> ebook.tex
-xelatex ebook.tex
-makeglossaries ebook
-xelatex ebook.tex
+build_item ebook
 mv ebook.pdf _build/ebook.pdf
 
 # make plain book file
 echo "\PassOptionsToPackage{disable}{todonotes} \documentclass{problemset}" > plain.tex
 awk 'FNR>2' main.tex >> plain.tex
-xelatex plain.tex
-makeglossaries plain
-xelatex plain.tex
+build_item plain
 mv plain.pdf _build/plain.pdf
 
 # make books with todos
 echo "\documentclass{problemset}" > todo.tex
 awk 'FNR>2' main.tex >> todo.tex
-xelatex todo.tex
-makeglossaries todo
-xelatex todo.tex
+build_item todo
 mv todo.pdf _build/todo.pdf
 
 rm ./ebook.tex
